77d3a78df469ebadb83807636428c80983248080
Артём Руденко <arudenko@elnis.ru>
2023-03-20 08:03:15 +0700

    add Layout

--- App.tsx
+++ App.tsx
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { NativeRouter, Route, Routes } from 'react-router-native';
 import SignUpPage from './src/pages/SignUpPage';
 import SignInPage from './src/pages/SignInPage';
+import Layout from './src/components/Layout/Layout';
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
       <NativeRouter>
-        <SignUpPage />
+        <Layout />
       </NativeRouter>
     </>
   );
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,57 @@
+import React, { FC } from "react";
+import { SafeAreaView, View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
+import { Link, Outlet } from "react-router-native";
+
+const Layout: FC = () => {
+
+    return (
+        <SafeAreaView style={styles.layout}>
+            <View style={styles.layout__main}>
+                <Outlet />
+            </View>
+            <View style={styles.layout__navigation}>
+                <TouchableOpacity style={styles.layout__buttonBox}>
+                    <Image style={styles.layout__profile} source={require("../../../assets/contacts.png")} />
+                    <Text>Контакты</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.layout__buttonBox}>
+                    <Image style={styles.layout__profile} source={require("../../../assets/chat.png")} />
+                    <Text>Чаты</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.layout__buttonBox}>
+                    <Image style={styles.layout__profile} source={require("../../../assets/profile.png")} />
+                    <Text>Профиль</Text>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    layout: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#ffcfba",
+    },
+    layout__main: {
+        flex: .9,
+    },
+    layout__navigation: {
+        width: "100%",
+        flex: .12,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-around",
+        backgroundColor: "#f07800",
+    },
+    layout__buttonBox: {
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    layout__profile: {
+        width: 35,
+        height: 35
+    }
+})
+
+export default Layout
