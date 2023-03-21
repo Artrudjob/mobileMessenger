9f1f30476b9eee8bcecac8bc9ba97b67767cbf9d
Артём Руденко <arudenko@elnis.ru>
2023-03-21 08:35:57 +0700

    install react-native-modal

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import { Provider } from 'react-redux';
 import { store } from './src/store/store';
 
 export default function App() {
-  const [user, setUser] = useState<boolean>(false);
+  const [user, setUser] = useState<boolean>(true);
 
   return (
     <>
